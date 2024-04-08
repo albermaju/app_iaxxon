0457cca5b3b619a5a5dd93dc30c2af6d3faabbc4
Alberto Maraña <albermaju@gmail.com>
2024-04-08 11:35:17 +0200

    Update Centro_de_Control_Iaxxon_Energía.py

--- Centro_de_Control_Iaxxon_Energía.py
+++ Centro_de_Control_Iaxxon_Energía.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_extras.app_logo import add_logo
-
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
 st.set_page_config(
     page_title="Centro de Control Iaxxon Energía",
@@ -15,7 +19,28 @@ st.markdown(
     """
    Aquí podrás monitorizar toda tu instalación y analizar como ha operado en los diferentes rangos de tiempo.
 
-    **👈 Selecciona tu instalación en el menú de la izquierda**
+    **Inicia sesión con tus credenciales:**
 
 """
 )
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    if username == 'jsmith':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 1')
+    elif username == 'rbriggs':
+        st.write(f'Welcome *{name}*')
+        st.title('Application 2')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
