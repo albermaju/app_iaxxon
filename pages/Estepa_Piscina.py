247c1d391792bd31d4db500e057e29c4815169fd
Alberto Maraña <albermaju@gmail.com>
2024-04-10 14:04:33 +0200

    Update Estepa_Piscina.py

--- pages/Estepa_Piscina.py
+++ pages/Estepa_Piscina.py
@@ -254,7 +254,8 @@ try:
     st.subheader("Temperatura Actual")
     col1, col2= st.columns(2)
     col1.metric("Temperatura",temp+temp_unit)
-    col2.metric("Climatología",st.image(url_png, width=50))
+    with col2:
+        st.image(url_png, width=50)
 
     st.subheader(" ")
 
