35a821a0fe6084317b992196dbb66d5986218d6e
Alberto Maraña <albermaju@gmail.com>
2024-05-06 14:03:49 +0200

    Update Estepa_Piscina.py

--- pages/Estepa_Piscina.py
+++ pages/Estepa_Piscina.py
@@ -29,7 +29,7 @@ MenuButtons(get_roles())
 #######################################
 # CONFIGURACIÓN DE PÁGINA
 #######################################
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     # Custom HTML/CSS for the banner
@@ -67,6 +67,100 @@ with col2:
     # Desplegable para seleccionar el período de tiempo
     time_period = st.selectbox('Selecciona el período de tiempo:', options)
 
+with col3:
+    st.subheader("Estado Actual")
+    #######################################
+    # API TIEMPO
+    #######################################
+    city="Estepa"
+    unit="Celsius"
+    speed="Kilometre/hour"
+    temp_unit=" °C"
+    wind_unit=" km/h"
+
+    api="9b833c0ea6426b70902aa7a4b1da285c"
+    url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
+    response=requests.get(url)
+    x=response.json()
+
+    try:
+        lon=x["coord"]["lon"]
+        lat=x["coord"]["lat"]
+        ex="current,minutely,hourly"
+        url2=f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={ex}&lang=sp&appid={api}'
+        res=requests.get(url2)
+        y=res.json()
+
+        maxtemp=[]
+        mintemp=[]
+        pres=[]
+        humd=[]
+        wspeed=[]
+        desc=[]
+        cloud=[]
+        rain=[]
+        dates=[]
+        sunrise=[]
+        sunset=[]
+        cel=273.15
+            
+        for item in y["daily"]:
+            if unit=="Celsius":
+                maxtemp.append(round(item["temp"]["max"]-cel,2))
+                mintemp.append(round(item["temp"]["min"]-cel,2))
+            else:
+                maxtemp.append(round((((item["temp"]["max"]-cel)*1.8)+32),2))
+                mintemp.append(round((((item["temp"]["min"]-cel)*1.8)+32),2))
+
+            if wind_unit=="m/s":
+                wspeed.append(str(round(item["wind_speed"],1))+wind_unit)
+            else:
+                wspeed.append(str(round(item["wind_speed"]*3.6,1))+wind_unit)
+
+            pres.append(item["pressure"])
+            humd.append(str(item["humidity"])+' %')
+                
+            cloud.append(str(item["clouds"])+' %')
+            rain.append(str(int(item["pop"]*100))+'%')
+
+            desc.append(item["weather"][0]["description"].title())
+
+        def bargraph():
+            fig=go.Figure(data=
+                [
+                go.Bar(name="Maximum",x=dates,y=maxtemp,marker_color='crimson'),
+                go.Bar(name="Minimum",x=dates,y=mintemp,marker_color='navy')
+                ])
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",barmode='group',margin=dict(l=70, r=10, t=80, b=80),font=dict(color="white"))
+            st.plotly_chart(fig)
+            
+        def linegraph():
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=dates, y=mintemp, name='Minimum '))
+            fig.add_trace(go.Scatter(x=dates, y=maxtemp, name='Maximimum ',marker_color='crimson'))
+            fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",font=dict(color="white"))
+            st.plotly_chart(fig)
+                
+        icon=x["weather"][0]["icon"]
+        current_weather=x["weather"][0]["description"].title()
+            
+        if unit=="Celsius":
+            temp=str(round(x["main"]["temp"]-cel,2))
+        else:
+            temp=str(round((((x["main"]["temp"]-cel)*1.8)+32),2))
+
+        url_png = f'http://openweathermap.org/img/w/{icon}.png'
+        
+        col1, col2, col3= st.columns(3)
+        col1.metric(f"Clima en {city}",temp+temp_unit)
+        with col2:
+            st.image(url_png, width=80)
+
+        st.subheader(" ")
+
+    except KeyError:
+        st.error("¡Ciudad no encontrada!")
+        
 #######################################
 # INFLUXDB
 #######################################
@@ -180,100 +274,7 @@ df.rename(columns = {'_time':'Tiempo'}, inplace = True)
 # DISEÑO PÁGINA STREAMLIT
 #######################################
 
-st.subheader("Estado Actual")
 
-#######################################
-# API TIEMPO
-#######################################
-
-city="Estepa"
-unit="Celsius"
-speed="Kilometre/hour"
-temp_unit=" °C"
-wind_unit=" km/h"
-
-api="9b833c0ea6426b70902aa7a4b1da285c"
-url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api}"
-response=requests.get(url)
-x=response.json()
-
-try:
-    lon=x["coord"]["lon"]
-    lat=x["coord"]["lat"]
-    ex="current,minutely,hourly"
-    url2=f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={ex}&lang=sp&appid={api}'
-    res=requests.get(url2)
-    y=res.json()
-
-    maxtemp=[]
-    mintemp=[]
-    pres=[]
-    humd=[]
-    wspeed=[]
-    desc=[]
-    cloud=[]
-    rain=[]
-    dates=[]
-    sunrise=[]
-    sunset=[]
-    cel=273.15
-        
-    for item in y["daily"]:
-        if unit=="Celsius":
-            maxtemp.append(round(item["temp"]["max"]-cel,2))
-            mintemp.append(round(item["temp"]["min"]-cel,2))
-        else:
-            maxtemp.append(round((((item["temp"]["max"]-cel)*1.8)+32),2))
-            mintemp.append(round((((item["temp"]["min"]-cel)*1.8)+32),2))
-
-        if wind_unit=="m/s":
-            wspeed.append(str(round(item["wind_speed"],1))+wind_unit)
-        else:
-            wspeed.append(str(round(item["wind_speed"]*3.6,1))+wind_unit)
-
-        pres.append(item["pressure"])
-        humd.append(str(item["humidity"])+' %')
-            
-        cloud.append(str(item["clouds"])+' %')
-        rain.append(str(int(item["pop"]*100))+'%')
-
-        desc.append(item["weather"][0]["description"].title())
-
-    def bargraph():
-        fig=go.Figure(data=
-            [
-            go.Bar(name="Maximum",x=dates,y=maxtemp,marker_color='crimson'),
-            go.Bar(name="Minimum",x=dates,y=mintemp,marker_color='navy')
-            ])
-        fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",barmode='group',margin=dict(l=70, r=10, t=80, b=80),font=dict(color="white"))
-        st.plotly_chart(fig)
-        
-    def linegraph():
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=dates, y=mintemp, name='Minimum '))
-        fig.add_trace(go.Scatter(x=dates, y=maxtemp, name='Maximimum ',marker_color='crimson'))
-        fig.update_layout(xaxis_title="Dates",yaxis_title="Temperature",font=dict(color="white"))
-        st.plotly_chart(fig)
-            
-    icon=x["weather"][0]["icon"]
-    current_weather=x["weather"][0]["description"].title()
-        
-    if unit=="Celsius":
-        temp=str(round(x["main"]["temp"]-cel,2))
-    else:
-        temp=str(round((((x["main"]["temp"]-cel)*1.8)+32),2))
-
-    url_png = f'http://openweathermap.org/img/w/{icon}.png'
-    
-    col1, col2, col3= st.columns(3)
-    col1.metric(f"Clima en {city}",temp+temp_unit)
-    with col2:
-        st.image(url_png, width=80)
-
-    st.subheader(" ")
-
-except KeyError:
-    st.error("¡Ciudad no encontrada!")
 
 col1, col2, col3 = st.columns(3)
 
