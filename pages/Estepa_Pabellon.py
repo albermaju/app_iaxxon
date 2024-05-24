52ff4bf9260026b91fe2c3979beb5027e0a10acb
Alberto Maraña <albermaju@gmail.com>
2024-05-24 12:26:56 +0200

    Update Estepa_Pabellon.py

--- pages/Estepa_Pabellon.py
+++ pages/Estepa_Pabellon.py
@@ -263,12 +263,12 @@ df = pd.concat(df, ignore_index=True)
 to_drop = ['result', 'table', '_measurement']
 df.drop(to_drop, inplace=True, axis=1)
 
-estado_ventilador = dffan["_value"].iloc[-1]  # Tomamos el último valor de la serie de tiempo
+estado_ventilador = dffan[["_value"]].iloc[-1]  # Tomamos el último valor de la serie de tiempo
 
 dfpump = query_api.query_data_frame(org=st.secrets.db_credentials.org, query=query_pump)
 dfpump = pd.concat(dfpump, ignore_index=True)
 st.dataframe(dfpump)
-estado_bomba = dfpump["_value"].iloc[-1]  # Tomamos el último valor de la serie de tiempo
+estado_bomba = dfpump[["_value"]].iloc[-1]  # Tomamos el último valor de la serie de tiempo
 
 
 
