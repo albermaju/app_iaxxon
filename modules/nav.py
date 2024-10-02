64cf0550b6b4ed87860ebf59a6269ca84803a7d3
Alberto Maraña <albermaju@gmail.com>
2024-10-02 20:06:40 +0200

    Update nav.py

--- modules/nav.py
+++ modules/nav.py
@@ -33,7 +33,7 @@ def Page9Nav():
     st.sidebar.page_link("pages/huetorvega_pabellon.py", label="Pabellón Huétor Vega")
 
 def Page10Nav():
-    st.sidebar.page_link("pages/toyota-hispaljarafe.py", label="Toyota Hispaljarafe")
+    st.sidebar.page_link("pages/Toyota-Hispaljarafe.py", label="Toyota Hispaljarafe")
 
 
 
