d587dc6d20047f66d19b4cc1111b3551dc4d6a70
Alberto Maraña <albermaju@gmail.com>
2024-10-21 11:39:43 +0200

    v5

--- Centro_de_Control_Iaxxon_Energía.py
+++ Centro_de_Control_Iaxxon_Energía.py
@@ -1,5 +1,96 @@
 import streamlit as st
-from pages.Centro_de_Control import show_page
+from streamlit import session_state as ss
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from modules.nav import MenuButtons
 
-# Ejecuta la página principal
-show_page()
+st.set_page_config(page_title="Centro de Control Iaxxon Energía", page_icon="https://i.imgur.com/JEX19oy.png", layout="wide", initial_sidebar_state= "auto")
+
+
+#######################################
+# CONFIGURACIÓN DE PÁGINA
+#######################################
+
+# Custom HTML/CSS for the banner
+custom_html = """
+<div class="banner">
+    <img src="https://i.imgur.com/SJQWq0F.png" alt="Banner Image">
+</div>
+<style>
+    .banner {
+	    margin: 30px auto;
+	    width: 40%;
+	    min-width: 230px;
+	    max-width: 330px;
+	    position: relative;
+	    height: auto;
+	    min-height: 300px;
+	    max-height: 500px;
+	    overflow: hidden;
+    }
+    .banner img {
+	    max-width : 330px;
+	    width: 100%;
+	    position: absolute;
+    }
+</style>
+"""
+# Display the custom HTML
+st.components.v1.html(custom_html)
+
+CONFIG_FILENAME = 'config.yaml'
+with open(CONFIG_FILENAME) as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+
+def get_roles():
+    """Gets user roles based on config file."""
+    with open(CONFIG_FILENAME) as file:
+        config = yaml.load(file, Loader=SafeLoader)
+
+    if config is not None:
+        cred = config['credentials']
+    else:
+        cred = {}
+
+    return {username: user_info['role'] for username, user_info in cred['usernames'].items() if 'role' in user_info}
+
+
+st.header('Centro de control')
+
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+login_tab, resetpassword_tab = st.tabs(['Iniciar Sesión', 'Nueva Contraseña'])
+
+with login_tab:
+    authenticator.login(location='main')
+
+    if ss["authentication_status"]:
+        authenticator.logout(location='main')    
+        st.write(f'Bienvenid@ *{ss["name"]}*')
+
+    elif ss["authentication_status"] is False:
+        st.error('Usuario o contraseña incorrectos')
+    elif ss["authentication_status"] is None:
+        st.warning('Por favor, introduzca tu usuario y contraseña')
+
+with resetpassword_tab:
+    if st.session_state["authentication_status"]:
+        try:
+            if authenticator.reset_password(st.session_state["username"]):
+                st.success('Contraseña modificada con éxito')
+        except Exception as e:
+            st.error(e)
+
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)      
+
+# Call this late because we show the page navigator depending on who logged in.
+MenuButtons(get_roles())
